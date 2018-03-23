c9824c58f09b90c2614b996e0880934a40227288
Owner <Owner@HP>
2018-03-23 10:19:28 +0000

    Add project files.

---
+++ CathysWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CathysWebsite.MvcApplication" Language="C#" %>
---
+++ CathysWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CathysWebsite.Startup))]
+namespace CathysWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
