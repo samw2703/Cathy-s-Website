b4936254af8d02f807f1e04abd703e0c54fda014
Sam <samw2703@gmail.com>
2018-03-23 11:31:12 +0000

    Add bootstrap-united

--- CathysWebsite/App_Start/BundleConfig.cs
+++ CathysWebsite/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace CathysWebsite
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-united.css",
                       "~/Content/site.css"));
         }
     }
