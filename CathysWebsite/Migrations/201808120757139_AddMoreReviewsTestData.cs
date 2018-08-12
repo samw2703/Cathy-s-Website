206e6c39afdfc8c757fc25e62bb2cba5f2772959
Sam <samw2703@gmail.com>
2018-08-12 11:41:31 +0100

    Reviews now display properly

---
+++ CathysWebsite/Migrations/201808120757139_AddMoreReviewsTestData.cs
@@ -0,0 +1,16 @@
+namespace CathysWebsite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMoreReviewsTestData : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
